74082da1cc057a9aca2176b42be364d1e187a7f6
muthukumar <hellomuthukumar@gmail.com>
2017-02-04 12:02:54 +0530

    Initial Code Setup for Java Collection Framework

---
+++ dn-j2se-collection-workout/src/org/dn/jcf/workout/HelloWorld.java
@@ -0,0 +1,14 @@
+package org.dn.jcf.workout;
+
+/**
+ * This is HelloWorld Program for Java Collection Framework (JCF)
+ * @author muthukumar thangavinayagam
+ * @Date 4/FEB/2017
+ */
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
